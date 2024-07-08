30b12a393371c212750e15a7cab8ec5267efff7f
shweta payghan <shwetapayghan3@gmail.com>
2024-07-08 16:15:26 +0530

    Test commit

--- src/test/java/testng/assertion/VerifySoftAssertExample.java
+++ src/test/java/testng/assertion/VerifySoftAssertExample.java
@@ -25,7 +25,6 @@ public class VerifySoftAssertExample {
         WebElement usernameTextBox = driver.findElement(By.xpath("//input[starts-with(@name,'name')]"));
         softAssert.assertTrue(usernameTextBox.isDisplayed(), "Username Textbox should be display");
         softAssert.assertAll();
-        System.out.println("Do Hard Work");
         driver.close();
 
 
